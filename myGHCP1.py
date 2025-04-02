92aba0caba089007a5e4117f68ba4567a0fcb4a8
Levy, Yoav <yoav.levy@intel.com>
2025-04-02 05:20:20 -0700

    hello - first draft commit

---
+++ myGHCP1.py
@@ -0,0 +1,8 @@
+#please write a short python code that will print out "hello world - this is my first VSCode with Github Copilot" to the console and terminal then also will creat a file with the same line in it
+
+print("hello world - this is my first VSCode with Github Copilot")
+
+with open("hello.txt", "w") as f:
+    f.write("hello world - this is my first VSCode with Github Copilot")
+
+
